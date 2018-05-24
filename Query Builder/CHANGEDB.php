b84c2dcb6bd519581b6548699832f308642c04a9
Sandra Kuipers <sandra@skuipers.com>
2018-05-24 11:16:51 +0800

    Add settings page, increment version to 1.5.00

--- Query Builder/CHANGEDB.php
+++ Query Builder/CHANGEDB.php
@@ -162,3 +162,12 @@ $sql[$count][1] = '';
 ++$count;
 $sql[$count][0] = '1.4.01';
 $sql[$count][1] = '';
+
+//v1.5.00
+++$count;
+$sql[$count][0] = '1.5.00';
+$sql[$count][1] = "
+INSERT INTO `gibbonSetting` (`scope` ,`name` ,`nameDisplay` ,`description` ,`value`)VALUES ('Query Builder', 'exportDefaultFileType', 'Default Export File Type', '', 'Excel2007');end
+INSERT INTO `gibbonAction` (`gibbonModuleID`, `name`, `precedence`, `category`, `description`, `URLList`, `entryURL`, `defaultPermissionAdmin`, `defaultPermissionTeacher`, `defaultPermissionStudent`, `defaultPermissionParent`, `defaultPermissionSupport`, `categoryPermissionStaff`, `categoryPermissionStudent`, `categoryPermissionParent`, `categoryPermissionOther`) VALUES ((SELECT gibbonModuleID FROM gibbonModule WHERE name='Query Builder'), 'Manage Settings', '0', 'Queries', 'Allows a privileged user to manage Query Builder settings.', 'settings_manage.php', 'settings_manage.php', 'Y', 'N', 'N', 'N', 'N', 'Y', 'N', 'N', 'N');end
+INSERT INTO `gibbonPermission` (`permissionID` ,`gibbonRoleID` ,`gibbonActionID`) VALUES (NULL , '1', (SELECT gibbonActionID FROM gibbonAction JOIN gibbonModule ON (gibbonAction.gibbonModuleID=gibbonModule.gibbonModuleID) WHERE gibbonModule.name='Query Builder' AND gibbonAction.name='Manage Settings'));end
+";
---
+++ Query Builder/settings_manageProcess.php
@@ -0,0 +1,60 @@
+<?php
+/*
+Gibbon, Flexible & Open School System
+Copyright (C) 2010, Ross Parker
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+include '../../gibbon.php';
+
+$URL = $_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.getModuleName($_POST['address']).'/settings_manage.php';
+
+if (isActionAccessible($guid, $connection2, '/modules/Query Builder/settings_manage.php') == false) {
+    $URL .= '&return=error0';
+    header("Location: {$URL}");
+    exit;
+} else {
+    //Proceed!
+    $exportDefaultFileType = (isset($_POST['exportDefaultFileType'])) ? $_POST['exportDefaultFileType'] : NULL;
+
+    //Validate Inputs
+    if (empty($exportDefaultFileType)) {
+        $URL .= '&return=error3';
+        header("Location: {$URL}");
+        exit;
+    } else {
+        //Write to database
+        $fail = false;
+
+        try {
+            $data = array('value' => $exportDefaultFileType);
+            $sql = "UPDATE gibbonSetting SET value=:value WHERE scope='Query Builder' AND name='exportDefaultFileType'";
+            $result = $connection2->prepare($sql);
+            $result->execute($data);
+        } catch (PDOException $e) {
+            $fail = true;
+        }
+
+        if ($fail == true) {
+            $URL .= '&return=error2';
+            header("Location: {$URL}");
+            exit;
+        } else {
+            $URL .= '&return=success0';
+            header("Location: {$URL}");
+            exit;
+        }
+    }
+}
